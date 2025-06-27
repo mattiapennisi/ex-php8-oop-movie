bebc3b1dac84a8503b3f633bb0cf27878c698cd3
mattiapennisi <mattiapennisi0@gmail.com>
2025-06-27 11:03:22 +0200

    Refactor Movie class and improve code formatting

--- index.php
+++ index.php
@@ -1,21 +1,39 @@
-<?php 
+<?php
 
+class Movie
+{
+    public $title;
+    public $description;
 
+    public function __construct($title, $description)
+    {
+
+        $this->title = $title;
+        $this->description = $description;
+    }
+};
+
+$movieJaws = new Movie('Jaws', 'Nice movie');
+$movieIntolerance = new Movie('Intolerance', 'Good old movie');
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OOP</title>
 </head>
+
 <body>
-    <?php 
-    
-    
-    
+    <?php
+
+    var_dump($movieJaws);
+    var_dump($movieIntolerance);
+
     ?>
 </body>
+
 </html>
