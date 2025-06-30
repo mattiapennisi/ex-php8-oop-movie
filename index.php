d129e5e41bcf3fb2ae6135ef842728e0d633ace2
mattiapennisi <mattiapennisi0@gmail.com>
2025-06-30 17:26:33 +0200

    Refactor index.php to enhance movie display layout; comment out var_dump for cleaner output

--- index.php
+++ index.php
@@ -14,15 +14,45 @@ include_once('./db.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OOP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
 
 <body>
 
     <?php
 
-    var_dump($movies);
+    // var_dump($movies);
 
     ?>
+
+    <div class="container-fluid">
+
+        <h1 class="text-center mt-4">Movies list</h1>
+
+        <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-3 my-4">
+            <?php 
+            
+            foreach ($movies as $movie) {
+                echo "
+                    <div class='col'>
+                        <div class='card' style='height: 250px; overflow: hidden;'>
+                            <div class='card-body'>
+                                <h4 class='card-title'>$movie->title</h4>
+                                <p class='card-text'>$movie->year</p>
+                                <p class='card-text'>$movie->description</p>
+                                <p class='card-text'>Rating: $movie->rating/10</p>
+                            </div>
+                        </div>
+                    </div>
+                ";
+            }
+            
+            ?>
+        </div>
+        
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 
 </body>
 
